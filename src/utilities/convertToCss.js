54c6784b1f179593bb786ddc0c6711047d8855e5
Anthony Collins <a.r.c_@hotmail.co.uk>
2020-08-15 16:19:33 +0100

    additional jsdoc stuff

--- src/utilities/convertToCss.js
+++ src/utilities/convertToCss.js
@@ -2,7 +2,9 @@ const css = require('css')
 
 /**
  * Convert an AST object to a CSS string
+ * @function
  * @param {object} astObject
+ * @returns {string} A valid CSS string
  */
 function convertToCss (astObject) {
   return css.stringify(astObject)
--- src/utilities/convertToJson.js
+++ src/utilities/convertToJson.js
@@ -2,7 +2,9 @@ const css = require('css')
 
 /**
  * Return an AST object converted from a CSS string
+ * @function
  * @param {string} cssString
+ * @returns {object} AST object
  */
 function convertToJson (cssString) {
   return css.parse(cssString)
